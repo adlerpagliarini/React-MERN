e54a527d3d4edc928871b95358ffb533401c2fe7
AdlerPagliarini <adlerpagliarini@gmail.com>
2018-07-29 11:25:15 -0300

    Fix-ReduxDevToolsExtension

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
 <h3>Redux-A CRUD Example</h3>
 <a href="https://codeburst.io/redux-a-crud-example-abb834d763c9">https://codeburst.io/redux-a-crud-example-abb834d763c9</a>
 
-#
+# Published at Heroku
+<a href="https://blooming-badlands-99148.herokuapp.com/">https://blooming-badlands-99148.herokuapp.com/</a>
 <img src="https://github.com/adlerpagliarini/React-MERN/blob/master/react-redux-mern.jpg" />
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -6,9 +6,20 @@ import thunk from 'redux-thunk';
 const initialState = {};
 const middleware = [thunk]; //thunk is a middleware
 
-const composingMiddlewareAndDevTools = compose (
+/*const composingMiddlewareAndDevTools = compose (
 	applyMiddleware(...middleware),
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);*/
+
+/*https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup*/
+const composeEnhancers =  typeof window === 'object' &&  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const composingMiddlewareAndDevTools = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
 );
 
 const store = createStore(rootReducer,
